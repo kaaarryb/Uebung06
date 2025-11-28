403009869f5f7aa69dcca7fbbb14110aa80ed73a
Karina <rybalchenkokatina310@gmail.com>
2025-11-28 01:06:52 +0100

    Добавил полный код проекта

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -1,5 +1,21 @@
 package h1;
 
 public class H1_main {
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 3, 4 };
+		int[] b = { 4, 3, 2, 1 };
+		boolean result = isMirrorArray(a, b);
+		System.out.println(result);
+	}
 
+	public static boolean isMirrorArray(int[] a, int[] b) {
+		if (a.length != b.length)
+			return false;
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] != b[b.length - 1 - i]) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -1,5 +1,30 @@
 package h3;
 
 public class H3_main {
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 3 };
+		int[] b = { 1, 3, 2 };
+		boolean result = compareArraysVal(a, b);
+		System.out.println(result);
+	}
 
+	public static boolean compareArraysVal(int[] a, int[] b) {
+		if (a.length != b.length)
+			return false;
+		for (int i = 0; i < a.length; i++) {
+			int countA = 0;
+			int countB = 0;
+			for (int j = 0; j < a.length; j++) {
+				if (a[j] == a[i])
+					countA++;
+			}
+			for (int j = 0; j < a.length; j++) {
+				if (b[j] == a[i])
+					countB++;
+			}
+			if (countA != countB)
+				return false;
+		}
+		return true;
+	}
 }
